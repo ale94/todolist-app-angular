29c3a3ff0a088a22b9483fd7c3a34cafd3f937a3
ale94 <alejandrorua111@gmail.com>
2025-08-09 11:20:34 -0300

    feat: add todo-dashboard component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoDashboard } from './components/todo-dashboard/todo-dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, TodoDashboard],
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('todolist-app');
---
+++ src/app/components/todo-dashboard/todo-dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-dashboard',
+  imports: [],
+  templateUrl: './todo-dashboard.html'
+})
+export class TodoDashboard {
+
+}
