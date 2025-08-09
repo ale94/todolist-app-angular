8ce7a3d477ecfc4357daafada9cc798b3dc7189a
ale94 <alejandrorua111@gmail.com>
2025-08-09 12:41:01 -0300

    feat: add tasks input

--- src/app/components/todo-dashboard/todo-dashboard.ts
+++ src/app/components/todo-dashboard/todo-dashboard.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { TodoInput } from "../todo-input/todo-input";
+import { Component, signal } from '@angular/core';
+import { TodoInput } from '../todo-input/todo-input';
 import { TodoTasks } from '../todo-tasks/todo-tasks';
+import { Task } from '../../interfaces/task';
 
 @Component({
   selector: 'todo-dashboard',
   imports: [TodoInput, TodoTasks],
-  templateUrl: './todo-dashboard.html'
+  templateUrl: './todo-dashboard.html',
 })
 export class TodoDashboard {
-
+  title = signal('Lista de Tareas');
+  tasks = signal<Task[]>([
+    {
+      name: 'Tarea 1',
+      completed: true,
+    },
+    {
+      name: 'Tarea 2',
+      completed: false,
+    },
+    {
+      name: 'Tarea 3',
+      completed: true,
+    },
+  ]);
 }
--- src/app/components/todo-tasks/todo-tasks.ts
+++ src/app/components/todo-tasks/todo-tasks.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Task } from '../../interfaces/task';
 
 @Component({
   selector: 'todo-tasks',
   imports: [],
-  templateUrl: './todo-tasks.html'
+  templateUrl: './todo-tasks.html',
 })
 export class TodoTasks {
-
+  tasks = input.required<Task[]>();
 }
